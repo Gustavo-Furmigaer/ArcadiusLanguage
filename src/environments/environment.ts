ba590682b5a5d084b107464f74074e68a41feced
Gustavo Furmigare <gustavofurmigare@gmail.com>
2025-05-26 11:37:55 -0400

    c

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,18 @@
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyChWmWbcDgu_NnV6_yv2ei1IEZintfMHto",
-    authDomain: "arcadius-language-8e1fa.firebaseapp.com",
-    projectId: "arcadius-language-8e1fa",
-    storageBucket: "arcadius-language-8e1fa.firebasestorage.app",
-    messagingSenderId: "645155703084",
-    appId: "1:645155703084:web:e7f4a60e63fd7752264489",
-  }
+// This file contains the Firebase configuration for the Arcadius Language project.
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChWmWbcDgu_NnV6_yv2ei1IEZintfMHto",
+  authDomain: "arcadius-language-8e1fa.firebaseapp.com",
+  projectId: "arcadius-language-8e1fa",
+  storageBucket: "arcadius-language-8e1fa.firebasestorage.app",
+  messagingSenderId: "645155703084",
+  appId: "1:645155703084:web:e7f4a60e63fd7752264489",
+  measurementId: "G-NXTPVKGQS2"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
