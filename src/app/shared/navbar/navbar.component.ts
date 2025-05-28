9360a414beb8baf5bccdb318d5b96c2c629f88b5
Gustavo Furmigaer <gustavofurmigare@gmail.com>
2025-05-28 13:10:41 -0400

    c

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  userLoggedIn = false;
 
-userLoggedIn = false;
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.afAuth.authState.subscribe(user => {
+      this.userLoggedIn = !!user;
+    });
+  }
 
-constructor(private afAuth: AngularFireAuth) {
-  this.afAuth.authState.subscribe(user => {
-    this.userLoggedIn = !!user;
-  });
-}
-
+  logout() {
+    this.afAuth.signOut().then(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
